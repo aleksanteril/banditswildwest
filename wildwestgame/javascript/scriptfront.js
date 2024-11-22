f1fcb4c846f2b6fd466b87d268ef5a8a7d5eb8ab
Xaruthh <jesse.mustonen@hotmail.fi>
2024-11-22 16:51:11 +0200

    js file example

--- wildwestgame/javascript/scriptfront.js
+++ wildwestgame/javascript/scriptfront.js
@@ -1,2 +1,17 @@
 //Backend yhteys on 127.0.0.1:3000 kautta
 //Endpointit määritetään tähän
+'use strict';
+
+async function loadUser(userId) {
+        const response = await fetch(`http://127.0.0.1:3000/users/${userId}`);
+}
+
+function displayUser(user) {
+    const userInfoDiv = document.querySelector('#user-info');
+    userInfoDiv.innerHTML = `
+        <h2>${user.isNew ? 'New User' : 'Returning User'}</h2>
+        <p>Name: ${user.name}</p>`;
+}
+
+const userId = 1;
+loadUser(userId);
